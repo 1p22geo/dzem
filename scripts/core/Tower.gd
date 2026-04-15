a325e9d02a17d8a51a863de30fb56d06592c4f97
Bartosz Bartocha <bartocha.bartosz@gmail.com>
2026-04-15 21:02:55 +0200

    getting vals to tower

--- scripts/core/Tower.gd
+++ scripts/core/Tower.gd
@@ -5,20 +5,12 @@ extends Tile
 
 var controller:EnemyController;
 var tower_sprite:Sprite2D;
-var range: int
-var cost: int 
-var texture: Texture
-var demage: int
-var tower_name: String
+
 
 func _ready() -> void:
 	controller = get_parent().get_parent().get_parent().get_node("EnemyController")
 	tower_sprite = get_node("TowerSprite")
-	range = tower.range
-	cost = tower.cost
-	demage = tower.demage
-	tower_name = tower.name
-	texture = tower.texture
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -26,30 +18,32 @@ func _process(delta: float) -> void:
 	AttackEnemy(closestEnemy)
 
 func FindClosestEnemyToAttack() -> Enemy:
-	var closestEnemy: Enemy;
-	var x_pos = tower_sprite.position.x
-	var y_pos = tower_sprite.position.y
-	var min_diff = 10000000
-	for enemy in controller.activeEnemies:
-		var x_pos_enemy = enemy.position.x
-		var y_pos_enemy = enemy.position.y
-	
-		var x_diff = abs(x_pos - x_pos_enemy)
-		var y_diff = abs(y_pos - y_pos_enemy)
+	if tower:
+		var closestEnemy: Enemy;
+		var x_pos = tower_sprite.position.x
+		var y_pos = tower_sprite.position.y
+		var min_diff = 10000000
+		for enemy in controller.activeEnemies:
+			var x_pos_enemy = enemy.position.x
+			var y_pos_enemy = enemy.position.y
 		
-		var diff = sqrt(x_diff*x_diff + y_diff*y_diff)
-		
-		if diff < min_diff && diff <= range:
-			min_diff = diff
-			closestEnemy = enemy
-	if (controller.activeEnemies.size() > 0):
-		print("enemy found")
-		return closestEnemy
-	else:
-		return null
+			var x_diff = abs(x_pos - x_pos_enemy)
+			var y_diff = abs(y_pos - y_pos_enemy)
+			
+			var diff = sqrt(x_diff*x_diff + y_diff*y_diff)
+			
+			if diff < min_diff && diff <= tower.range:
+				min_diff = diff
+				closestEnemy = enemy
+		if (controller.activeEnemies.size() > 0):
+			print("enemy found")
+			return closestEnemy
+		else:
+			return null
+	return null
 		
 func AttackEnemy(enemy:Enemy) -> void:
-	if enemy == null:
-		return 
-	enemy.hp -= demage
-	
+	if tower:
+		if enemy == null:
+			return 
+		enemy.hp -= tower.demage
