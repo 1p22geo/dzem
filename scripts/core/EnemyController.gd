4785badf2de0f3958c6f671c8b01012286fa4002
1p22geo <1p22geodecki@gmail.com>
2026-04-15 19:20:51 +0200

    temp for cojak

--- scripts/core/EnemyController.gd
+++ scripts/core/EnemyController.gd
@@ -4,9 +4,18 @@ extends Node
 @export var waveDefs: Waves;
 
 var time:float = 0;
+var wave_time:float = 0;
+var enemy_time:float = 0;
+
 var wave_no = 0;
+var enemies_spawned = 0;
+
+var state : #TODO
 
 
+
+func progress_waves() -> void:
+	pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
