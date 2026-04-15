dddd1412dc738567736d8c4694e05084677f3ee8
Filip Jastrzebski <1p22jastrzebski@gmail.com>
2026-04-15 20:29:39 +0200

    fix settings menu

--- scripts/ui/PauseMenu.gd
+++ scripts/ui/PauseMenu.gd
@@ -2,8 +2,6 @@ extends Control
 
 @onready var main_buttons: VBoxContainer = %MainButtons
 @onready var settings_panel: PanelContainer = %SettingsPanel
-@onready var volume_slider: HSlider = %VolumeSlider
-@onready var fullscreen_check: CheckButton = %FullscreenCheck
 
 
 func _ready() -> void:
@@ -11,9 +9,7 @@ func _ready() -> void:
 	%SettingsButton.pressed.connect(_on_settings_pressed)
 	%MainMenuButton.pressed.connect(_on_main_menu_pressed)
 	%ExitButton.pressed.connect(_on_exit_pressed)
-	%BackButton.pressed.connect(_on_back_pressed)
-	volume_slider.value_changed.connect(_on_volume_changed)
-	fullscreen_check.toggled.connect(_on_fullscreen_toggled)
+	settings_panel.back_pressed.connect(_on_settings_back)
 
 	visible = false
 
@@ -28,8 +24,6 @@ func _unhandled_input(event: InputEvent) -> void:
 
 
 func _pause() -> void:
-	volume_slider.value = db_to_linear(AudioServer.get_bus_volume_db(0))
-	fullscreen_check.button_pressed = DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN
 	settings_panel.hide()
 	main_buttons.show()
 	visible = true
@@ -47,6 +41,7 @@ func _on_resume_pressed() -> void:
 
 func _on_settings_pressed() -> void:
 	main_buttons.hide()
+	settings_panel.refresh()
 	settings_panel.show()
 
 
@@ -59,17 +54,6 @@ func _on_exit_pressed() -> void:
 	get_tree().quit()
 
 
-func _on_back_pressed() -> void:
+func _on_settings_back() -> void:
 	settings_panel.hide()
 	main_buttons.show()
-
-
-func _on_volume_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(0, linear_to_db(value))
-
-
-func _on_fullscreen_toggled(toggled_on: bool) -> void:
-	if toggled_on:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-	else:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
