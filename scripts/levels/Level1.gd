72b2521d74db10896420d17644f2ac2552a0deeb
Vladislavius17 <wladek1701.1000@gmail.com>
2026-04-16 08:04:09 +0200

    we are CHARLIE KIRK

--- scripts/levels/Level1.gd
+++ scripts/levels/Level1.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	GameManager.scales = 100
+	GameManager.scales = 250
 	GameManager.hp = 100
 
 
