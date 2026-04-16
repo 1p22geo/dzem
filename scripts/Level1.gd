6e6e1a6a793296de8d81ba3ff070c60b5576ecca
1p22geo <1p22geodecki@gmail.com>
2026-04-16 04:25:31 +0200

    fix: scales now reset on level start

---
+++ scripts/Level1.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	GameManager.scales = 100
+	GameManager.hp = 100
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
